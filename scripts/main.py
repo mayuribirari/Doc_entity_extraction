9f52f495e7ff76d36db84572c92dbd94cbdb2ca0
Aman2909 <amanpathak2909@gmail.com>
2020-03-13 11:13:29 +0530

    Preprocessing v2.0

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,8 @@
 import cv2
 import scripts.preprocessing
 import numpy as np
-PATH = '..\images\j.jpg'
+
+PATH = '..\\images\\j.jpg'
 image_org = cv2.imread(PATH)
 
 ##################### PREPROCESSING ####################
@@ -11,7 +12,6 @@ image = np.copy(image_org)
 image = scripts.preprocessing.detect_orientation(image)
 image = scripts.preprocessing.straighten(image)
 image = scripts.preprocessing.extract_image(image)
-scripts.preprocessing.plot_before_after(image_org,image)
+scripts.preprocessing.plot_before_after(image_org, image)
 
 #################### OCR ##############################
-
